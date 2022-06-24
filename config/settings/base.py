8196ba30d193984e234f511755c0235ce4122946
david shindra <davidshindra.dev@gmail.com>
2022-06-24 23:08:05 +0300

    Add app Core

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my apps
+    "apps.core.apps.CoreConfig",
     "apps.accounts.apps.AccountsConfig",
 ]
 

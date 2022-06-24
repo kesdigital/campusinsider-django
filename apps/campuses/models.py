3b9c42b55f6f3a85586a2051d42988e13367a44f
david shindra <davidshindra.dev@gmail.com>
2022-06-25 01:09:07 +0300

    Add model Campus
    Campus
    - campus_id
    - name
    - slug
    - created_at
    - updated_at

--- apps/campuses/models.py
+++ apps/campuses/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from apps.core.models import SluggedModel, TimestampedModel
+
+
+class Campus(SluggedModel, TimestampedModel):
+    campus_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta(TimestampedModel.Meta):
+        db_table = "campus"
+        verbose_name = "campus"
+        verbose_name_plural = "campuses"
--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-
 admin.site.site_header = "Campus Insider admin"
 admin.site.site_title = "Campus Insider admin"
 admin.site.index_title = "Campus Insider admin"

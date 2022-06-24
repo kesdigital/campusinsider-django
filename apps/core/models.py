f90e7e77562dee709e312baffa2cfe6a14f2dc9c
david shindra <davidshindra.dev@gmail.com>
2022-06-25 00:22:35 +0300

    Add abstract models SluggedModel, TimestampedModel
    SluggedModel
    - name
    - slug
    
    TimestampedModel
    - created_at
    - updated_at

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at"]
+
+
+class SluggedModel(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+    slug = models.SlugField(max_length=160, unique=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)

7119b4aa2b95e586bb1398b59a0a30b8e883dc54
david shindra <davidshindra.dev@gmail.com>
2022-06-26 14:07:04 +0300

    Add profile content and details to people/profile_detail.html

--- apps/people/views.py
+++ apps/people/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse, Http404
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import View
 
@@ -12,6 +12,9 @@ class ProfileListView(View):
 
 class ProfileDetailView(View):
     def get(self, request: HttpRequest, slug) -> HttpResponse:
-        profile = get_object_or_404(Profile, slug=slug)
+        try:
+            profile = Profile.objects.select_related("campus", "main_role").get(slug=slug)
+        except Profile.DoesNotExist:
+            raise Http404("Profile does not exist")
         context = {"profile": profile}
         return render(request, "people/profile_detail.html", context)

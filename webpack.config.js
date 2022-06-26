70e057ee401cf609ff938583c9675b949498f9aa
david shindra <davidshindra.dev@gmail.com>
2022-06-26 14:59:44 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    base: path.resolve(__dirname, "src/ts/base.ts"),
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, "static/js/"),
+    filename: "[name].min.js",
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  }
+}

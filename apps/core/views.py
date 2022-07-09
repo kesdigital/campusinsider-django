f1ec27e811585ba5213a508cd481ae61fbe6d4e1
david shindra <davidshindra.dev@gmail.com>
2022-07-09 21:58:31 +0300

    Add styles for articles on the home page

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,11 +2,17 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from apps.posts.models import Post
 from apps.profiles.models import FeaturedProfile
 
 
 class HomeView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         featured_profiles = FeaturedProfile.objects.select_related("profile")[:6]
-        context = {"featured_profiles": featured_profiles}
+        featured_posts = (
+            Post.objects.select_related("author")
+            .order_by("-published_at")
+            .filter(is_featured=True, status=Post.PUBLISHED)[:3]
+        )
+        context = {"featured_profiles": featured_profiles, "featured_posts": featured_posts}
         return render(request, "core/home.html", context)

d3be3619f78846f2ca60fb0b88bc5f8ce0170078
david shindra <davidshindra.dev@gmail.com>
2022-07-09 11:43:53 +0300

    Register Profile, Photo and FeaturedProfile models in Admin

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, FeaturedProfile, Photo
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("name", "profile_type")
+    list_filter = ("profile_type",)
+
+
+@admin.register(FeaturedProfile)
+class FeaturedProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass

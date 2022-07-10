8a63b5fec85055df160d0d544592e4a032eb923a
david shindra <davidshindra.dev@gmail.com>
2022-07-10 14:22:44 +0300

    Add post_list view and template

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+from django.http import HttpRequest, HttpResponse
+
+from .models import Post
+
+
+class PostListView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        posts = Post.objects.filter(status=Post.PUBLISHED).select_related("tags", "author")
+        context = {"posts": posts}
+        return render(request, "posts/post_list.html", context)
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),
+    path("posts/", include("apps.posts.urls")),
     path("profiles/", include("apps.profiles.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

679d6ac58c17eb08d2fd5885a65458e84adca134
david shindra <davidshindra.dev@gmail.com>
2022-07-10 14:57:10 +0300

    Create app entertainment

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-
-from django.views import View
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views import View
 
-from .models import Post
+from .models import Post, Tag
 
 
 class PostListView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        posts = Post.objects.filter(status=Post.PUBLISHED).select_related("tags", "author")
-        context = {"posts": posts}
+        tags = Tag.objects.all()
+        posts = Post.objects.filter(status=Post.PUBLISHED).select_related("author").prefetch_related("tags")
+        context = {"posts": posts, "tags": tags}
         return render(request, "posts/post_list.html", context)
--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "apps.profiles.apps.ProfilesConfig",
     "apps.accounts.apps.AccountsConfig",
     "apps.campuses.apps.CampusesConfig",
+    "apps.entertainment.apps.EntertainmentConfig",
 ]
 
 MIDDLEWARE = [
